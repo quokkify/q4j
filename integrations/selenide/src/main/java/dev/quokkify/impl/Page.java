7234bcd2cac3965b115b9bc8bad53c95dbc11ad2
Yaraslau Lazakovich <ylazakovich@gmail.com>
2026-08-10 14:06:03 +0200

    refactor(selenide)!: remove unused public API BaseBlock, ColorFormatter, PageTitle (#453)

--- integrations/selenide/src/main/java/dev/quokkify/impl/Page.java
+++ integrations/selenide/src/main/java/dev/quokkify/impl/Page.java
@@ -1,6 +1,5 @@
 package dev.quokkify.impl;
 
-import dev.quokkify.annotation.PageTitle;
 import dev.quokkify.annotation.PageUrl;
 import dev.quokkify.formatter.RegexFormatter;
 import dev.quokkify.parser.RegexParser;
@@ -11,25 +10,6 @@ import com.codeborne.selenide.WebDriverRunner;
  * Interface for Ui page class.
  */
 public interface Page {
-
-  /**
-   * Get title of page.
-   *
-   * @param args dynamic parts in url
-   * @return title as {@link String}
-   */
-  default String getTitle(Object... args) {
-    return getTitle().formatted(args);
-  }
-
-  /**
-   * Get title of page.
-   *
-   * @return title as {@link String}
-   */
-  default String getTitle() {
-    return getClass().getAnnotation(PageTitle.class).value();
-  }
 
   /**
    * Get url pattern with dynamic parts.
